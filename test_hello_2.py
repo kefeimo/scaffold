c1e985a4e61644c814af79dcc6aab212ffb220eb
Kefei <kefeimo@gmail.com>
2021-11-12 08:16:40 +0000

    fail on purpose

--- test_hello_2.py
+++ test_hello_2.py
@@ -6,3 +6,5 @@ class TestHello(TestCase):
     def test_hello_add(self):
         self.assertEqual(4, hello.add(3))
         
+    def test_hello_add_fail(self):
+        self.assertEqual(5, hello.add(3))
